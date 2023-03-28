6e23e31de4e87c2bb9198b822aa26dc06b8cf08e
IvanKahinga <ikahinga@gmail.com>
2023-03-28 13:15:12 +0300

    Added resources

---
+++ app/models/hero_power.rb
@@ -0,0 +1,6 @@
+class HeroPower < ApplicationRecord
+    belongs_to :power
+    belongs_to :hero
+    validates :strength, inclusion: {in:['average', 'weak', 'strong',"Average","Weak", 'Strong']}
+ 
+end
